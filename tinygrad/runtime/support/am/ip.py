9bb3eedbe7e2efd89f173cfdb7057635449d3146
elkoled <elkoled@gmail.com>
2026-09-25 15:35:13 -0700

    am: flush_tlb gives up after 1s

--- tinygrad/runtime/support/am/ip.py
+++ tinygrad/runtime/support/am/ip.py
@@ -118,7 +118,8 @@ class AM_GMC(AM_IP):
 
       self.adev.reg(f"reg{ip}VM_INVALIDATE_ENG17_REQ").write(req, inst=inst)
 
-      wait_cond(lambda: self.adev.reg(f"reg{ip}VM_INVALIDATE_ENG17_ACK").read(inst=inst) & (1 << vmid), value=(1 << vmid), msg="flush_tlb timeout")
+      wait_cond(lambda: self.adev.reg(f"reg{ip}VM_INVALIDATE_ENG17_ACK").read(inst=inst) & (1 << vmid), value=(1 << vmid), timeout_ms=1000,
+                msg="flush_tlb timeout")
 
       if ip == "MM": self.adev.regMMVM_INVALIDATE_ENG17_SEM.write(0x0, inst=inst)
       if self.adev.ip_ver[am.GC_HWIP] >= (11,0,0) and ip == "MM":
